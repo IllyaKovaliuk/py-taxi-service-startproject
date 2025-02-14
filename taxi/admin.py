3a1abd30bba873968a0e53287c49ddf376fbbd38
Illya Kovaliuk <kovalukilla271@gmail.com>
2025-02-14 16:18:27 +0200

    Add .gitignore to ignore unnecessary files

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from taxi.models import Driver, Manufacturer, Car
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_filter = ["manufacturer"]
+    search_fields = ["model"]
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = ("id", "first_name", "last_name", "license_number")
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
--- taxi/models.py
+++ taxi/models.py
@@ -26,3 +26,6 @@ class Driver(AbstractUser):
     password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.username},( {self.first_name} {self.last_name})"
