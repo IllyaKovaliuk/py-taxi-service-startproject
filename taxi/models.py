3657752f1e06b497e8b90bace65402bf305c953a
Illya Kovaliuk <kovalukilla271@gmail.com>
2025-02-14 13:40:28 +0200

    Додав всі файли

---
+++ taxi/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from taxi_service import settings
+from django.contrib.auth.models import AbstractUser
+
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} - {self.country}"
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return f"{self.manufacturer} - {self.model}"
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
